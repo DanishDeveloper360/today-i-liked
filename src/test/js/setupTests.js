97f2ee7b32a8e5d920a0a366d743a0a0c2be97f6
m.browne@elsevier.com <m.browne@elsevier.com>
2020-05-01 15:20:51 +0200

    remove electron and fix dependencies in tests

--- src/test/js/setupTests.js
+++ src/test/js/setupTests.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import 'components/dist/scopus-components';
-import 'stylesheet/dist/scopus-stylesheet';
+import '@scopus/components';
+import '@scopus/stylesheet';
 
 Enzyme.configure({ adapter: new Adapter() });
